a292e5fc1b1823802aaf6b0f54ac608f933ec3d9
josephtheengineer <joseph@theengineer.life>
2020-12-28 01:14:30 +0000

    refactor: update chunk.tools

--- src/scripts/chunk/tools.gd
+++ src/scripts/chunk/tools.gd
@@ -1,12 +1,20 @@
 #warning-ignore:unused_class_variable
 const meta := {
-	script_name = "world.chunk_tools",
+	script_name = "chunk.tools",
 	description = """
 		
 	"""
 }
 
-static func get_chunk_sub(location: int): #############################################
+
+# chunk.tools.get_chunk_sub ####################################################
+const get_chunk_sub_meta := {
+	func_name = "chunk.tools.get_chunk_sub",
+	description = """
+		
+	""",
+		}
+static func get_chunk_sub(location: int, args := get_chunk_sub_meta) -> int: ###
 	var x = 0
 	if location == 0:
 		return 0
@@ -17,14 +25,32 @@ static func get_chunk_sub(location: int): ######################################
 		while !(location <= x and location > x*16):
 			x -= 1
 	return x - 1
+# ^ chunk.tools.get_chunk_sub ##################################################
 
 
-static func get_chunk(location: Vector3): #############################################
+# chunk.tools.get_chunk ########################################################
+const get_chunk_meta := {
+	func_name = "chunk.tools.get_chunk",
+	description = """
+		
+	""",
+		}
+static func get_chunk(location: Vector3, args := get_chunk_meta) -> Vector3: ###
 	var x = get_chunk_sub(int(round(location.x))+8)
 	var y = get_chunk_sub(int(round(location.y))+8)
 	var z = get_chunk_sub(int(round(location.z))+8)
 	
 	return Vector3(x, y, z)
+# ^ chunk.tools.get_chunk ######################################################
 
-static func get_local_chunk_pos(location: Vector3, chunk_location: Vector3):
+
+# chunk.tools.get_local_chunk_pos ##############################################
+const get_local_chunk_pos_meta := {
+	func_name = "chunk.tools.get_local_chunk_pos",
+	description = """
+		
+	""",
+		}
+static func get_local_chunk_pos(location: Vector3, chunk_location: Vector3, args := get_local_chunk_pos_meta) -> Vector3:
 	return location - chunk_location*16
+# ^ chunk.tools.get_local_chunk_pos ##############################################
