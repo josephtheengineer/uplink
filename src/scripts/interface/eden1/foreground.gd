503c84ed8852334422cbde5dccd36768ef096c91
josephtheengineer <joseph@theengineer.life>
2021-01-14 23:22:42 +0000

    feature: implement eden2 worlds menu

--- src/scripts/interface/eden1/foreground.gd
+++ src/scripts/interface/eden1/foreground.gd
@@ -8,6 +8,8 @@ const meta := {
 	"""
 }
 
+func _ready():
+	update_worlds_list()
 
 func _on_delete_button_down():
 	var button: TextureButton = get_node("HBoxTop/Center/Delete")
@@ -62,15 +64,15 @@ func _on_download_button_up():
 func _on_left_button_down():
 	var button: TextureButton = get_node("HBoxMiddle/Center/Left")
 	button.rect_min_size = button.rect_min_size - Vector2(20, 20)
-	var script = Core.scripts.chunk.generator
-	for function in script.get_script_method_list():
-		if script.get(function.name + "_meta") != null:
-			print(function.name)
 
 
 func _on_left_button_up():
 	var button: TextureButton = get_node("HBoxMiddle/Center/Left")
 	button.rect_min_size = button.rect_min_size + Vector2(20, 20)
+	
+	if index - 1 >= 0 and index - 1 < worlds_list.size():
+		index -=1
+		update_worlds_list()
 
 
 func _on_right_button_down():
@@ -81,5 +83,54 @@ func _on_right_button_down():
 func _on_right_button_up():
 	var button: TextureButton = get_node("HBoxMiddle/Center2/Right")
 	button.rect_min_size = button.rect_min_size + Vector2(20, 20)
+	
+	if index + 1 >= 0 and index + 1 < worlds_list.size():
+		index +=1
+		update_worlds_list()
 
+var worlds_list := [
+	"test1",
+	"test2",
+	"test3"
+]
 
+var index := 0
+
+func update_worlds_list():
+	var worlds: HBoxContainer = get_node("HBoxMiddle/VBox/Worlds")
+	var status: RichTextLabel = get_node("HBoxMiddle/VBox/Status")
+	var world_name: RichTextLabel = get_node("HBoxMiddle/VBox/Name")
+	var unselected_texture := load("res://aux/assets/textures/ui/eden2/ipad/main_menu/ipad~world_unselected.png")
+	var selected_texture := load("res://aux/assets/textures/ui/eden2/ipad/main_menu/ipad~world_selected.png")
+	
+	status.bbcode_text = "[center] Choose a world to load [/center]"
+	world_name.bbcode_text = "[center] " + worlds_list[index] + " [/center]"
+	
+	
+	for button in worlds.get_children():
+		remove_child(button)
+		button.queue_free()
+	
+	for i in range(-2, 3):
+		var button := TextureButton.new()
+		var center := CenterContainer.new()
+		center.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		center.size_flags_vertical = Control.SIZE_EXPAND_FILL
+		
+		if i == 0:
+			button.texture_normal = selected_texture
+		elif index + i >= 0 and index + i < worlds_list.size():
+			button.texture_normal = unselected_texture
+		
+		button.rect_min_size = Vector2(50, 50)
+		button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		button.size_flags_vertical = Control.SIZE_EXPAND_FILL
+		button.stretch_mode = TextureButton.STRETCH_KEEP_CENTERED
+		center.add_child(button)
+		worlds.add_child(center)
+
+func list_functions(script):
+	#var script = Core.scripts.chunk.generator
+	for function in script.get_script_method_list():
+		if script.get(function.name + "_meta") != null:
+			print(function.name)
